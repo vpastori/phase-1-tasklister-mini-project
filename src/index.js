5cdb246b07e270cf89042fa5e438fd8c1c4e42b6
Vincent Pastorino <vpastori@DESKTOP-TDG3FUD>
2023-04-10 21:41:44 -0400

    finished lab

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  document.querySelector('form').addEventListener('submit', (e) => {
+    e.preventDefault()
+    const input = document.querySelector('input[type="text"]');
+    handleInput(input.value);
+  })
 });
+
+function handleInput(input){
+  let p = document.createElement('p')
+  p.textContent = input
+  document.querySelector('#tasks').appendChild(p)
+}
